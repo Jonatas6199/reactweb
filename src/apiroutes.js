a7f493c4734cc91d74d9c3da670105979555e2c0
Jonatas6199 <jojokl@hotmail.com>
2020-10-22 22:43:41 -0300

    adiciona axios na requisição de login

--- src/apiroutes.js
+++ src/apiroutes.js
@@ -1,22 +1,29 @@
 //const express = require('express');
-//const axios = require('axios');
+var axios = require('axios');
 //const app = express();
 //const cors = require('cors');
-const route = "https://gps-indoor.herokuapp.com/";
+var route = "http://localhost:5000/";
 
 //app.use(cors());
 
 async function Login(user, password){
-   
-    const loginEnd = route+"user";
-
-    var userInfo =  "Basic " +  Buffer.from(user + ":" + password).toString('base64')
-
-    var request = new XMLHttpRequest();
-    request.open("GET",loginEnd);
-    request.setRequestHeader("authorization",userInfo);
-    request.send();
-    console.log(request.response);
+    var loginEnd = route+"user/";
+    var userInfo =  "Basic " +  Buffer.from(user + ":" + password).toString('base64');
+    var config = {
+        headers: {'authorization': userInfo}
+      };
+    axios.get(loginEnd,config).then((response) => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+      });
+   // var request = new XMLHttpRequest();
+   // request.open("GET",loginEnd);
+   // request.setRequestHeader("authorization",userInfo);
+    //await request.send();
 }
 
 export default Login
+
