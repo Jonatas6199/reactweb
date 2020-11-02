ce1683df114e135a669abe2f22c0bc900ddc66b7
Jonatas6199 <jojokl@hotmail.com>
2020-11-02 17:41:42 -0300

    Adiciona loginpage.js

---
+++ src/loginpage.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Login from './apiroutes.js';
+
+function getUsername(){
+
+    var value =  document.getElementById("username").value;
+    return value;
+}
+
+function getPassword(){
+    var value =  document.getElementById("password").value;
+    return value;
+}
+
+function DoLogin(){
+    var username = getUsername();
+    var password = getPassword();
+    Login(username, password);
+}
+function LoginPage(){
+    return(
+        <div className="App-style">
+        <div className="Form-style" >
+            <div className= "transbox">
+                 <label className="stgletters" >
+                             Usuário:
+                            <p></p>
+                            <input  id="username" type="text" name="name" />
+                 </label>
+                    <div margin="10px">
+                        <label className="stgletters">
+                            <p></p>
+                             Senha:
+                            <p></p>
+                            <input id="password" type="password" name="password" />
+                        </label>
+                    </div>
+                        <p></p>
+                    <div opacity= "1">
+                         <button className="button" onClick= {DoLogin} > Login
+                         </button>
+                    </div>
+            </div>
+        </div>
+
+    </div>
+    )
+}
+
+export default LoginPage
