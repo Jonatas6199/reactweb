da514c2db2b3507e99c04b94baf53a9555e0e5de
Jonatas6199 <jojokl@hotmail.com>
2020-11-02 17:42:36 -0300

    remove código do login da route.js e transfere para novo arquivo JS

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
-import Login from './apiroutes.js';
+import HomePage from './homepage.js';
+import LoginPage from './loginpage';
 
 /*definição das rotas : abaixo */ 
 
-function getUsername(){
-
-    var value =  document.getElementById("username").value;
-    return value;
-}
-
-function getPassword(){
-    var value =  document.getElementById("password").value;
-    return value;
-}
-
-function DoLogin(){
-    var username = getUsername();
-    var password = getPassword();
-    Login(username, password);
-}
 const Routes = () => (
 
     /*dá a habilidade para trabalhar com as rotas do browser, entender a url, etc...*/
@@ -27,35 +12,13 @@ const Routes = () => (
    <BrowserRouter>
        
         <Switch>
-           <Route  path="/" component ={() => 
-           <div className="App-style">
-                <div className="Form-style" >
-                    <div className= "transbox">
-                         <label className="stgletters" >
-                                     Usuário:
-                                    <p></p>
-                                    <input  id="username" type="text" name="name" />
-                         </label>
-                            <div margin="10px">
-                                <label className="stgletters">
-                                    <p></p>
-                                     Senha:
-                                    <p></p>
-                                    <input id="password" type="password" name="password" />
-                                </label>
-                            </div>
-                                <p></p>
-                            <div opacity= "1">
-                                 <button className="button" onClick= {DoLogin} > Login
-                                 </button>
-                            </div>
-                    </div>
-                </div>
-      
-            </div>}/>
-            <Route exact path="/moveblocks" component ={() => <h1>Hello World</h1>}/>
+            <Route exact={true} path="/" component ={LoginPage}/> 
+            <Route exact={true} path="/moveblocks" component ={() => <h1>Hello World</h1>}/>
+            <Route exact={true} path="/home" component={HomePage} /> 
         </Switch>
     </BrowserRouter>
 );
 
 export default Routes;
+
+//
