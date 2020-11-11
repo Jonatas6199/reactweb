de8b694280839988305c25029c05a17ad0cf3802
Jonatas6199 <jojokl@hotmail.com>
2020-11-10 21:24:41 -0300

    Add privateroute.js

---
+++ src/privateroute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Redirect ,Route } from 'react-router-dom'
+import authentication from './authentication.js';
+
+export default function PrivateRoute(props){
+  if (authentication.authenticated()){
+    return(
+      <Route exact={true} path={props.path} component={props.component}/>)
+  }
+  else{
+    return (
+    <Redirect to ='/login'/>)
+  }
+}
+
