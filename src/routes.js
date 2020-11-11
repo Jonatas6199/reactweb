26bc79fc64b243d2949ed45bd5637ce3b7d02432
Jonatas6199 <jojokl@hotmail.com>
2020-11-10 21:25:08 -0300

    troca rotas públicas por privadas

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import HomePage from './homepage.js';
 import LoginPage from './loginpage';
-
+import PrivateRoute from './privateroute.js';
 /*definição das rotas : abaixo */ 
 
 const Routes = () => (
@@ -10,11 +10,10 @@ const Routes = () => (
     /*dá a habilidade para trabalhar com as rotas do browser, entender a url, etc...*/
    /*o switch tem a função de não deixar mais de uma rota ser chamada ao mesmo tempo*/
    <BrowserRouter>
-       
         <Switch>
-            <Route exact={true} path="/" component ={LoginPage}/> 
-            <Route exact={true} path="/moveblocks" component ={() => <h1>Hello World</h1>}/>
-            <Route exact={true} path="/home" component={HomePage} /> 
+            <Route exact={true} path="/login" component ={LoginPage}/> 
+            <PrivateRoute exact={true} path="/moveblocks" component ={() => <h1>Hello World</h1>}/>
+            <PrivateRoute exact={true} path="/" component={HomePage} /> 
         </Switch>
     </BrowserRouter>
 );
